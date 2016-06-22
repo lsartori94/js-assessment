c35199f035686db7a220539346ba61d7e326b077
Luca Sartori <luca.sartori@globant.com>
2016-06-21 21:35:03 -0300

    arrays done

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,113 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
+    indexOf: function(arr, item) {
+        return arr.indexOf(item);
+    },
 
-  },
+    sum: function(arr) {
+        var tot = 0;
+        arr.forEach(function(item) {
+            tot += item;
+        });
+        return tot;
+    },
 
-  sum: function(arr) {
+    remove: function(arr, item) {
+        var newArr = arr.filter(function(elem) {
+            return elem !== item;
+        });
+        return newArr;
+    },
 
-  },
+    removeWithoutCopy: function(arr, item) {
+        var length = arr.length;
+        for (var i = 0; i < length; i++) {
+            if (arr[i] === item) {
+                arr.splice(i, 1);
+                i--;
+                length--;
+            }
+        }
+        return arr;
+    },
 
-  remove: function(arr, item) {
+    append: function(arr, item) {
+        arr.push(item);
+        return arr;
+    },
 
-  },
+    truncate: function(arr) {
+        arr.splice(arr.length - 1, 1);
+        return arr;
+    },
 
-  removeWithoutCopy: function(arr, item) {
+    prepend: function(arr, item) {
+        arr.unshift(item);
+        return arr;
+    },
 
-  },
+    curtail: function(arr) {
+        arr.splice(0, 1);
+        return arr;
+    },
 
-  append: function(arr, item) {
+    concat: function(arr1, arr2) {
+        return arr1.concat(arr2);
+    },
 
-  },
+    insert: function(arr, item, index) {
+        var arr2 = [];
+        arr.forEach(function(elem) {
+            if (arr.indexOf(elem) == index) {
+                arr2.push(item);
+            }
+            arr2.push(elem);
+        });
+        return arr2;
+    },
 
-  truncate: function(arr) {
+    count: function(arr, item) {
+        var sum = 0;
+        arr.forEach(function(elem) {
+            if (elem === item) {
+                sum++;
+            }
+        });
+        return sum;
+    },
 
-  },
+    duplicates: function(arr) {
+        // Filter only duplicates
+        var answ = arr.filter(function(elem) {
+            return arraysAnswers.count(arr, elem) > 1;
+        });
 
-  prepend: function(arr, item) {
+        var toRet = [];
+        answ.forEach(function(elem) {
+            if (arraysAnswers.count(toRet, elem) === 0) {
+                toRet.push(elem);
+            }
+        });
 
-  },
+        return toRet;
+    },
 
-  curtail: function(arr) {
+    square: function(arr) {
+        var arr2 = [];
+        arr.forEach(function(elem) {
+            arr2.push(elem * elem);
+        });
+        return arr2;
+    },
 
-  },
-
-  concat: function(arr1, arr2) {
-
-  },
-
-  insert: function(arr, item, index) {
-
-  },
-
-  count: function(arr, item) {
-
-  },
-
-  duplicates: function(arr) {
-
-  },
-
-  square: function(arr) {
-
-  },
-
-  findAllOccurrences: function(arr, target) {
-
-  }
+    findAllOccurrences: function(arr, target) {
+        var answ = [];
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] === target) {
+                answ.push(i);
+            }
+        }
+        return answ;
+    }
 };
