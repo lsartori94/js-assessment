1701e33717a72eff131872d6c9b057415c6fea3c
lsartori <lucasartori94@gmail.com>
2016-06-21 21:36:25 -0300

    Asyc started

--- app/async.js
+++ app/async.js
@@ -2,7 +2,13 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+      return new Promise(function(resolve, reject){
+         if (value) {
+             resolve("Yeah!");
+         } else {
+             reject("Nope"):
+         }
+      });
   },
 
   manipulateRemoteData: function(url) {
