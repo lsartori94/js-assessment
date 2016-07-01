836c61c3e3cd80b2e5ccca6cf2920ce74a11d7ee
Luca Sartori <luca.sartori@globant.com>
2016-07-01 11:18:27 -0300

    Async done

--- app/async.js
+++ app/async.js
@@ -1,17 +1,33 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
-      return new Promise(function(resolve, reject){
-         if (value) {
-             resolve("Yeah!");
-         } else {
-             reject("Nope"):
-         }
-      });
-  },
+    async: function(value) {
+        return new Promise(function(resolve, reject) {
+            if (value) {
+                resolve(value);
+            } else {
+                reject(value);
+            }
+        });
+    },
 
-  manipulateRemoteData: function(url) {
-
-  }
+    manipulateRemoteData: function(url) {
+        return new Promise(function(resolve, reject) {
+            var request = new XMLHttpRequest();
+            request.open("GET", url);
+            request.onload = function() {
+                if (request.status == 200) {
+                    array = [];
+                    result = JSON.parse(request.responseText);
+                    for (var i = 0; i < result.people.length; i++) {
+                        array.push(result.people[i].name);
+                    }
+                    resolve(array.sort());
+                } else {
+                    reject(false);
+                }
+            };
+            request.send();
+        });
+    }
 };
