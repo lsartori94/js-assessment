4805564a1933607ac355a5184629ff94636f3c01
Luca Sartori <luca.sartori@globant.com>
2016-07-01 17:23:54 -0300

    functions half done

--- app/functions.js
+++ app/functions.js
@@ -1,39 +1,53 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray: function(fn, arr) {
+    argsAsArray: function(fn, arr) {
+        return fn.apply(fn, arr);
+    },
 
-  },
+    speak: function(fn, obj) {
+        return fn.apply(obj);
+    },
 
-  speak: function(fn, obj) {
+    functionFunction: function(str) {
+        return function(str2) {
+            return str + ', ' + str2;
+        };
+    },
 
-  },
+    makeClosures: function(arr, fn) {
+        var res = [];
 
-  functionFunction: function(str) {
+        var sqr = function(x) {
+            return function() {
+                return fn(x);
+            };
+        };
+        arr.forEach(function(elem) {
+            res.push(sqr(elem));
+        });
+        return res;
+    },
 
-  },
+    partial: function(fn, str1, str2) {
+        return function(punctuation) {
+            return fn.call(this, str1, str2, punctuation);
+        };
+    },
 
-  makeClosures: function(arr, fn) {
+    useArguments: function() {
 
-  },
+    },
 
-  partial: function(fn, str1, str2) {
+    callIt: function(fn) {
 
-  },
+    },
 
-  useArguments: function() {
+    partialUsingArguments: function(fn) {
 
-  },
+    },
 
-  callIt: function(fn) {
+    curryIt: function(fn) {
 
-  },
-
-  partialUsingArguments: function(fn) {
-
-  },
-
-  curryIt: function(fn) {
-
-  }
+    }
 };
